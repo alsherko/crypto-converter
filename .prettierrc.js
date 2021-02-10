7f47808dbfd3d81b238bcd06b4e39eb05b10b117
alsherko <al.sherko@gmail.com>
2021-02-10 17:03:15 +0300

    add eslint and prettier

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  useTabs: false,
+  semi: false,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  jsxBracketSameLine: false,
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import React from 'react'
+import {
+  Container,
+  createStyles,
+  Grid,
+  makeStyles,
+  Paper,
+  Theme,
+} from '@material-ui/core'
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      padding: theme.spacing(10),
+    },
+    paper: {
+      padding: theme.spacing(2),
+    },
+  }),
+)
 
 function App() {
+  const classes = useStyles()
+
   return (
-    <div className="App"></div>
-  );
+    <Container className={classes.root} maxWidth="lg">
+      <Grid container spacing={3}>
+        <Grid item xs={8}>
+          <Paper className={classes.paper}></Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}></Paper>
+        </Grid>
+      </Grid>
+    </Container>
+  )
 }
 
-export default App;
+export default App
